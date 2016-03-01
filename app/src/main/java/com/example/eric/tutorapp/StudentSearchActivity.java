24ca2f8a75e22b7ac542e53c97d63ef81ae25ae0
Eric Romrell <eric_romrell@byu.edu>
2016-03-01 12:06:29 -0700

    Added firebase and autocomplete interaction for courses

--- app/src/main/java/com/example/eric/tutorapp/StudentSearchActivity.java
+++ app/src/main/java/com/example/eric/tutorapp/StudentSearchActivity.java
@@ -1,25 +1,68 @@
 package com.example.eric.tutorapp;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Spinner;
+import android.widget.Button;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class StudentSearchActivity extends AppCompatActivity {
-    private static final String[] COURSES = new String[] {"CS 142", "CS 224", "CS 235", "CS 236", "CS 240"};
+    private static final String TAG = "StudentSearchActivity";
+
+    private Firebase myCoursesRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_student_search);
 
-        AutoCompleteTextView courseText = (AutoCompleteTextView) findViewById(R.id.courseText);
-        courseText.setAdapter(new ArrayAdapter<>(this, R.layout.course_spinner_item, COURSES));
+        final ProgressDialog dialog = ProgressDialog.show(StudentSearchActivity.this, "Loading Courses", "Please wait...");
+
+        myCoursesRef = new Firebase("https://romrell4-tutorapp.firebaseio.com/courses");
+        myCoursesRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<String> courseList = new ArrayList<>();
+                Iterable<DataSnapshot> courses = dataSnapshot.getChildren();
+                for (DataSnapshot department : courses) {
+                    for (DataSnapshot course : department.getChildren()) {
+                        courseList.add(department.getKey() + " " + course.child("catalogNumber").getValue() + " - " + course.child("transcriptTitle").getValue());
+                    }
+                }
+                Log.d(TAG, "onDataChange: Got all info!");
+
+                final AutoCompleteTextView courseText = (AutoCompleteTextView) findViewById(R.id.courseText);
+                courseText.setAdapter(new ArrayAdapter<>(StudentSearchActivity.this, R.layout.course_spinner_item, courseList));
+
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
