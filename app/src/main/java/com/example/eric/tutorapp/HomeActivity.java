52f3e03d66905cca117cc1abaa27b6b0a6c9f9f2
Eric Romrell <eric_romrell@byu.edu>
2016-03-07 11:45:54 -0700

    Fixed the home page

--- app/src/main/java/com/example/eric/tutorapp/HomeActivity.java
+++ app/src/main/java/com/example/eric/tutorapp/HomeActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
@@ -19,8 +20,11 @@ public class HomeActivity extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home);
 
+        TextView continueAsText = (TextView) findViewById(R.id.continueAsText);
+        continueAsText.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/alegreya-sans-sc/AlegreyaSansSC-Regular.ttf"));
+
         Button studentButton = (Button) findViewById(R.id.studentButton);
-//        studentButton.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/hk-grotesk/HKGrotesk-Regular.otf"));
+        studentButton.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/hk-grotesk/HKGrotesk-Regular.otf"));
         studentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
